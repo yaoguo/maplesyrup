771edb9969cd2518ac10e872196f9bc5d8276de4
yaoguo <guorunyao@gmail.com>
2014-11-24 11:37:18 +0100

    assertion

--- src/org/wahlzeit/model/location/MapcodeLocation.java
+++ src/org/wahlzeit/model/location/MapcodeLocation.java
@@ -5,56 +5,41 @@ import java.util.List;
 import com.mapcode.*;
 
 public class MapcodeLocation extends AbstractLocation {
-	private String mapcode;
-	private final String MAPCODE_DELIMITER = ".";
-	
 
 	private List <Mapcode> mapcodes;
 	
-	public MapcodeLocation(String photoPath)
+	public MapcodeLocation()
 	{
-		setLatAndLong(photoPath);
-		this.mapcodes = MapcodeCodec.encode(this.latitude, this.longitude);
-		this.coordinate = createNewCoordinate();
+
 	}
 	
-	protected String createNewCoordinate() {
-		// TODO Auto-generated method stub
-		return this.mapcodes.get(this.mapcodes.size()-1).toString();
+	protected String createMapcodeLocation() {
+		//assertion the list is not empty
+		
+		
+		return mapcodes.get(mapcodes.size()-1).toString();
 	}
 	
 	
 	@Override
 	public String getLocation() {
 		
-		return mapcode;
+		return location;
 	}
 
 	@Override
 	public void setLocation(String mapcode) {
+		//assert mapcode is valid
 		
 		
 		
+		location = mapcode;
 	}
 
 	@Override
-	public void setLocation(double latitude, double longitude) {
-		//no implementation for GPS location in MapcodeLocation class
-	}
-	
-	/**
-	* get mapcode
-	* @methodtype getter
-	*/
-	public String getMapcode() {
-		return mapcode;
-	}
-	
-	/**
-	* set mapcode
-	* @methodtype setter
-	*/
-	public void setMapcode(String mapcode) {
-		this.mapcode = mapcode;
+	public void setLocation(double gpslatitude, double gpslongitude) {
+		//mapcode can be set with gps coordinate
+		mapcodes = MapcodeCodec.encode(gpslatitude, gpslongitude);
+		location = createMapcodeLocation();
 	}
 }
