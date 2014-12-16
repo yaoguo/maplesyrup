25caba9853e7e9dd0f4d2ccfbbcefd3356d02700
yaoguo <guorunyao@gmail.com>
2014-12-16 11:49:14 +0100

    modified ModelMain

--- src/org/wahlzeit/main/ModelMain.java
+++ src/org/wahlzeit/main/ModelMain.java
@@ -25,6 +25,8 @@ import java.io.FileFilter;
 import java.sql.*;
 
 import org.wahlzeit.model.*;
+import org.wahlzeit.model.domain.MaplesyrupFactory;
+import org.wahlzeit.model.domain.MaplesyrupTypeManager;
 import org.wahlzeit.services.*;
 import org.wahlzeit.servlets.AbstractServlet;
 import org.wahlzeit.webparts.*;
@@ -185,6 +187,9 @@ public abstract class ModelMain extends AbstractMain {
 		PhotoCaseManager.getInstance().savePhotoCases();
 		PhotoManager.getInstance().savePhotos();			
 		UserManager.getInstance().saveUsers();
+		
+		MaplesyrupTypeManager.getInstance().saveMaplesyrupTypes();
+		MaplesyrupFactory.getInstance();
 
 		saveGlobals();
 	}
